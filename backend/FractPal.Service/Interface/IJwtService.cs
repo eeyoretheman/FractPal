75eb3305ea14f4de6e4f10f2892355fa6b4155de
Yordan Todorov <yordan.iliev.todorov@gmail.com>
2026-02-20 18:09:59 +0200

    chore: comments for the jwt service

--- backend/FractPal.Service/Interface/IJwtService.cs
+++ backend/FractPal.Service/Interface/IJwtService.cs
@@ -2,7 +2,15 @@ namespace FractPal.Service.Interface;
 
 using FractPal.Model.Entities;
 
+/// <summary>
+/// Provides JWT token generation for authenticated users.
+/// </summary>
 public interface IJwtService
 {
+    /// <summary>
+    /// Generates a signed JWT access token for the given user.
+    /// </summary>
+    /// <param name="user">The user for whom the token will be issued.</param>
+    /// <returns>A signed JWT string.</returns>
     public string GenerateToken(FractPalUser user);
 }
