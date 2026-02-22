7315ff7f17c62f6ea98e3a8de9a2f1cb82c2fb43
mirotoko <mkavrykov@gmail.com>
2026-02-22 10:03:43 +0200

    feat: migrated changes

--- backend/FractPal.Data/ApplicationDbContext.cs
+++ backend/FractPal.Data/ApplicationDbContext.cs
@@ -9,6 +9,7 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Fractal> Fractals { get; set; }
     public DbSet<Like> Likes { get; set; }
     public DbSet<Follow> Follows { get; set; }
+    public DbSet<Comment> Comments { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -63,6 +64,24 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 
             // Unique constraint: can't follow same user twice
             entity.HasIndex(f => new { f.FollowerId, f.FollowingId }).IsUnique();
+        });
+
+        // Comment configuration
+        builder.Entity<Comment>(entity =>
+        {
+            entity.HasKey(c => c.Id);
+            entity.HasOne(c => c.User)
+                  .WithMany(u => u.Comments)
+                  .HasForeignKey(c => c.UserId)
+                  .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasOne(c => c.Fractal)
+                  .WithMany(f => f.Comments)
+                  .HasForeignKey(c => c.FractalId)
+                  .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasIndex(c => c.FractalId);
+            entity.HasIndex(c => c.UserId);
         });
     }
 }
